7e391dd3c8b71ab66c77ae657e04c5ec2fad933f
mojo1643 <johnrejimathai@gmail.com>
2019-03-30 01:46:19 -0500

    student user login views

--- code/app/controllers/users_controller.rb
+++ code/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def user_login
+    end
+
+    def user_login_post
+    end
+
+    def user_signup
+    end
+
+    def user_signup_post
+    end
 end
