457fb8d8790f5c699b23da6ff9c28c2708d33456
Venkata <marinenivenkatanaidu@gmail.com>
2019-04-25 04:21:56 +0000

    Improvement of views for statistics

--- code/app/controllers/statistics_controller.rb
+++ code/app/controllers/statistics_controller.rb
@@ -7,8 +7,10 @@ class StatisticsController < ApplicationController
         topic_chosen = params[:topic_to_display]
         if topic_chosen == 'all'
             @problems = Problem.all
+            @topic_chosen = 'All Topics'
         else 
             @problems = Problem.where(topic: [topic_chosen])
+            @topic_chosen = Topic.find(topic_chosen).name
         end
     end
     
@@ -18,16 +20,20 @@ class StatisticsController < ApplicationController
     
     def reset_by_topic
         topic_chosen = params[:topic_to_reset]
+        text_to_display = ''
         if topic_chosen == 'all'
             @problems = Problem.all
+            text_to_display = 'all topics'
         else 
             @problems = Problem.where(topic: [topic_chosen])
+            text_to_display = Topic.find(topic_chosen).name
         end
         @problems.each do |problem|
             problem.correct_attempts=0
             problem.num_of_attempts=0
             problem.save!
         end
+        flash[:success] = "Statistics reset for "+ text_to_display
         redirect_to reset_stats_path
     end
 
